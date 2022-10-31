2b59dc5564eae2b69ab851e9e901a5389e3037f6
mariekart42 <schwokelbaer@gmail.com>
2022-10-31 19:15:51 +0100

    sesam

--- files/main.c
+++ files/main.c
@@ -6,7 +6,7 @@
 /*   By: mmensing <mmensing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 12:52:51 by mmensing          #+#    #+#             */
-/*   Updated: 2022/10/31 17:01:45 by mmensing         ###   ########.fr       */
+/*   Updated: 2022/10/31 19:00:46 by mmensing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int main(void)
     void *mlx_win = 0;
     t_data x;
     
+    // case 1 (P1[30/HIGHT-30], P2[510/HIGHT-160]) => WORKS
+    // case 2 (P1[/], P2[/])
     
     x.mlx = mlx;
     x.mlx_win = mlx_win;
@@ -25,21 +27,21 @@ int main(void)
     x.mlx = mlx_init();
 	x.mlx_win = mlx_new_window(x.mlx, WIDHT, HIGHT, "bullshit world!");
     
-    init_x(&x, 30, 110);
-    init_y(&x, HIGHT-30, HIGHT-60);
+    init_x(&x, 30, 510);
+    init_y(&x, HIGHT-30, HIGHT-160);
     
     // put_cross(&x, 10, HIGHT - 10);
     // put_cross(&x, 40, HIGHT - 10);
     // put_cross(&x, 40, HIGHT - 40);
     // put_cross(&x, 500,HIGHT - 300);
 
-    put_cross(&x, 30, HIGHT-30);
-    put_cross(&x, 110, HIGHT-60);
+    put_cross(&x, 100, HIGHT-100);
+    put_cross(&x, 800, HIGHT-150);
     // put_cross(&x, 40, 10);
     // put_cross(&x, 40, 40);
     // put_cross(&x, 650,650);
     
-    put_line(&x, 0xeec900);
+    // put_line(&x, 0xeec900);
     
     mlx_loop(x.mlx);
 }
